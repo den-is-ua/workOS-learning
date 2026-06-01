832097562ec3f911bcd50f5a056b6cd00fbdc449
admin <admin@admins-MacBook-Pro.local>
2026-06-01 07:38:54 +0300

    reinstall prjs

--- app2/app/Models/User.php
+++ app2/app/Models/User.php
@@ -9,13 +9,25 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
+#[Fillable(['name', 'email', 'workos_id', 'avatar'])]
+#[Hidden(['workos_id', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    /**
+     * Get the user's initials.
+     */
+    public function initials(): string
+    {
+        return Str::of($this->name)
+            ->explode(' ')
+            ->map(fn (string $name) => Str::of($name)->substr(0, 1))
+            ->implode('');
+    }
 
     /**
      * Get the attributes that should be cast.
